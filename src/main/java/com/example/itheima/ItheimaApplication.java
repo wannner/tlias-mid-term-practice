35e5f27e0435d81bf90f4f5f662ae53438c8f919
wannner <2636420049@qq.com>
2024-01-17 21:16:08 +0800

    基本环境搭建成功

---
+++ src/main/java/com/example/itheima/ItheimaApplication.java
@@ -0,0 +1,13 @@
+package com.example.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItheimaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItheimaApplication.class, args);
+    }
+
+}
